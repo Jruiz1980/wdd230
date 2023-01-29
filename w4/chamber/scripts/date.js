e8b993835152de9b28c864158c246a7d4064784b
Jruiz1980 <johnathanruiz@gmail.com>
2023-01-29 02:32:43 -0500

    add chnages

--- w4/chamber/scripts/date.js
+++ w4/chamber/scripts/date.js
@@ -1,11 +1,5 @@
-const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside"); 
+
+const datefieldUK = document.querySelector(".date"); 
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-datefield.innerHTML = `<em>${fulldate}</em>`;
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
