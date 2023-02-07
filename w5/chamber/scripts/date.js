843684008a27c923e22b20ca9b718cdcb5e4d111
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-07 07:37:20 -0500

    add date

--- w5/chamber/scripts/date.js
+++ w5/chamber/scripts/date.js
@@ -3,3 +3,11 @@ const datefieldUK = document.querySelector(".date");
 const now = new Date();
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+const eventSection = document.querySelector(".event");
+
+if (datefieldUK == Monday || Tuesday){
+eventSection
+}
+else {
+    eventSection.style.display = "block"
+}
