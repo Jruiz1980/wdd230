adced03075711602724ccf35f0a8994e45556dcf
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-12 01:57:14 -0500

    add changes

--- w6/chamber/scripts/windchill.js
+++ w6/chamber/scripts/windchill.js
@@ -1,6 +1,6 @@
-var temp= document.getElementById("#temperature");
-var wSpeed= document.getElementById("#windSpeed");
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+var temp = document.getElementsByClassName("temperature");
+var wSpeed = document.getElementsByClassName("windSpeed");
+var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
 var windChill= Math.round(windChill);
 document.getElementById("windChill").innerHTML= windChill;
