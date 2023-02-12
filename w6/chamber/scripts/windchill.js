1bbb5617bddee8222e45dd1b0b32e187710cf812
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-12 02:06:52 -0500

    save

--- w6/chamber/scripts/windchill.js
+++ w6/chamber/scripts/windchill.js
@@ -1,5 +1,5 @@
-var temp = document.getElementsByClassName("temperature");
-var wSpeed = document.getElementsByClassName("windSpeed");
+var temp = document.querySelector("temperature");
+var wSpeed = document.querySelector("windSpeed");
 var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
 var windChill= Math.round(windChill);
