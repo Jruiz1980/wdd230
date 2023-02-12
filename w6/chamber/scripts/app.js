249cb19c3e1f445bd6ca8dbe1a98cd5bc71d2849
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-12 01:38:28 -0500

    add js

--- w6/chamber/scripts/app.js
+++ w6/chamber/scripts/app.js
@@ -5,3 +5,23 @@ let d = new Date();
 let lastUpdated = document.lastModified;
 
     document.getElementById("lastModified").textContent = lastUpdated;
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
+
+const datefieldUK = document.querySelector(".date"); 
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+const eventSection = document.querySelector(".event");
+
+if (now == 1 || now == 2){
+eventSection;
+}
+else {
+    eventSection.style.display = "none";
+}
--- w6/chamber/scripts/btn.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById('hamburgerBtn').classList.toggle("open");
-}
-const x = document.getElementById('hamburgerBtn')
-x.onclick = toggleMenu;
