2a9ecdde7cd667d9ba2dd1e0179ed450a534cb79
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-13 08:09:37 -0500

    add changes

--- w6/chamber/scripts/windchill.js
+++ w6/chamber/scripts/windchill.js
@@ -1,6 +1,5 @@
-var temp = document.querySelector("temperature");
-var wSpeed = document.querySelector("windSpeed");
-var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+var temp = document.querySelector(".temperature").innerHTML;
+var wSpeed = document.querySelector(".windSpeed").innerHTML;
+var windChiller = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-var windChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= windChill;
+document.querySelector(".windChill").innerHTML= windChiller;
--- w6/js/scripts.js
+++ w6/js/scripts.js
@@ -1,6 +1,6 @@
-var temp= document.querySelector("high");
-var wSpeed= document.querySelector("wSpeed");
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+var temp= document.querySelector(".high");
+var wSpeed= document.querySelector(".wSpeed");
+var windChill= 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-var windChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= windChill;
+
+document.querySelector("#windChill").innerHTML= windChill;
