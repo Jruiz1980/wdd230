84c20fe015db52d15c5f3150936dd6a3dacba3a2
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-13 18:20:10 -0500

    add temp

--- w6/chamber/scripts/chill.js
+++ w6/chamber/scripts/chill.js
@@ -1,18 +1,6 @@
-/*let temp = document.querySelector(".temperature").innerHTML;
-let wSpeed = document.querySelector(".windSpeed").innerHTML;
-var windChiller = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
+const temp = document.querySelector("#temperature").innerHTML;
+const wSpeed = document.querySelector("#windSpeed").innerHTML;
+var windChill = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-var windChiller = Math.round(windChiller);
-document.querySelector(".windChill").innerHTML= windChiller;*/
-
-function windChill(tempF, speed) 
-{
-    var tempF = parseFloat(document.getElementById("temperature").value);
-    var speed = parseFloat(document.getElementById("windSpeed").value);
-    var windChillF = windChill(tempF,speed);
-    document.getElementById("windChill").innerHTML = windChillF;
-
-    var windChill = 35.74 + 0.6215 * tempF - 35.75 * speed**0.16 + 0.4275 * tempF * speed**0.16;
-    return windChill.toFixed(0);
-}
-return windChill;
+var windChill = Math.round(windChill);
+document.querySelector("#windChill").innerHTML= windChill;
