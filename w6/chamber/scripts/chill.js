5b60211c9aa5431dbbd9ce7b154aa8230fb201f4
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-13 08:41:04 -0500

    add changes

--- w6/chamber/scripts/chill.js
+++ w6/chamber/scripts/chill.js
@@ -2,4 +2,5 @@ let temp = document.querySelector(".temperature").innerHTML;
 let wSpeed = document.querySelector(".windSpeed").innerHTML;
 var windChiller = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
+var windChiller = Math.round(windChiller);
 document.querySelector(".windChill").innerHTML= windChiller;
