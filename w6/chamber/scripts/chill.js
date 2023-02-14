5071b5b8be82dc97f2292fb663bb9278e004da46
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-13 23:27:01 -0500

    add range

--- w6/chamber/scripts/chill.js
+++ w6/chamber/scripts/chill.js
@@ -3,4 +3,10 @@ const wSpeed = document.querySelector("#windSpeed").innerHTML;
 var windChill = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
 var windChill = Math.round(windChill);
+
+if (temp <=50 && wSpeed > 3){
 document.querySelector("#windChill").innerHTML= windChill;
+}
+else{
+    document.querySelector("#windChill").innerHTML=NaN;
+}
