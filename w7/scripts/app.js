99310c07d77143a484e48144bea83fcd1bcc3316
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-17 01:53:17 -0500

    add image load

--- w7/scripts/app.js
+++ w7/scripts/app.js
@@ -1,28 +1,30 @@
-const images = document.querySelectorAll("img[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+const imagesOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
 
 const loadImages = (image) => {
-    image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
-    };
-};
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {image.removeAttribute('data-src')};
+}
 
-
-if ("IntersectionObserver" in window) {
+if ("IntersectionObserver" in window) 
+{
     const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-
-            }
+    items.forEach((item) => {
+        if (item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+        }
         });
-    });
-    images.forEach((img) => {
+    },imagesOptions);
+    imagesToLoad.forEach((img) => {
         observer.observe(img);
     });
-} else {
-    images.forEach((img) => {
+} else 
+{
+    imagesToLoad.forEach((img) => {
         loadImages(img);
     });
 }
