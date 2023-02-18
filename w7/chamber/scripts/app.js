e3a333ac3c04c235be128f4693becb98ef44d6e8
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-18 00:29:13 -0500

    add new week files

---
+++ w7/chamber/scripts/app.js
@@ -0,0 +1,31 @@
+
+let d = new Date();
+
+    document.getElementById("currentYear").textContent = d.getFullYear();
+
+let lastUpdated = document.lastModified;
+
+    document.getElementById("lastModified").textContent = lastUpdated;
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+    x.onclick = toggleMenu;
+
+
+const datefieldUK = document.querySelector(".date"); 
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+    datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+const curr = new Date().getDay();
+const eventSection = document.querySelector(".event");
+if (curr == 1 || curr == 2)
+    {
+    eventSection;
+    }
+else {
+    eventSection.style.display = "none";
+    }
