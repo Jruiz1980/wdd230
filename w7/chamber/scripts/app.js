a280084014c711db0f2e49a4fedf20597624c095
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-18 17:41:22 -0500

    add images

--- w7/chamber/scripts/app.js
+++ w7/chamber/scripts/app.js
@@ -29,3 +29,31 @@ if (curr == 1 || curr == 2)
 else {
     eventSection.style.display = "none";
     }
+
+
+const images = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    images.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    images.forEach((img) => {
+        loadImages(img);
+    });
+}
