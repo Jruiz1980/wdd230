3e476b48af0ced104251dcd4bbae5a6bc8c3af87
Jruiz1980 <johnathanruiz@gmail.com>
2023-02-28 20:47:37 -0500

    add changes

---
+++ w7/chamber/scripts/form.js
@@ -0,0 +1,26 @@
+const a = document.queryselector(".free");
+const b = document.queryselector(".bronze");
+const c = document.querySelector(".silver");
+const d = document.queryselector(".gold");
+const v = docuemnt.querySelector(".00");
+const w = docuemnt.querySelector(".01");
+const x = docuemnt.querySelector(".02");
+const y = docuemnt.querySelector(".03");
+const z = document.querySelector(".04");
+
+switch (membership){
+    case w:
+        a
+        break;
+    case x:
+        b
+        break;
+    case y:
+        c
+        break;
+    case z:
+        d
+        break;
+    default:
+        v;
+};
