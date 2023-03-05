a3c22225bd3533b73a306ffbb834cea4c2499178
Jruiz1980 <johnathanruiz@gmail.com>
2023-03-04 23:10:42 -0500

    add changes

---
+++ w7/chamber/scripts/app2.js
@@ -0,0 +1,21 @@
+
+let d = new Date();
+
+    document.getElementById("currentYear").textContent = d.getFullYear();
+
+let lastUpdated = document.lastModified;
+
+    document.getElementById("lastModified").textContent = lastUpdated;
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById('hamburgerBtn').classList.toggle("open");
+}
+const x = document.getElementById('hamburgerBtn')
+    x.onclick = toggleMenu;
+
+
+const datefieldUK = document.querySelector(".date"); 
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+    datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
