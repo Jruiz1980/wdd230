f6cc1f5dbfff9ac2cdc4929a47bfe8481abbf591
Jruiz1980 <johnathanruiz@gmail.com>
2023-03-10 01:18:18 -0500

    add changes

--- w10/Weather API/scripts/weather.js
+++ w10/Weather API/scripts/weather.js
@@ -8,7 +8,7 @@ document.querySelector("#last-update").textContent = `Last Updated: ${document.l
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=3f3f013e4fed008181e53a4db70ffb93units=imperial';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=3f3f013e4fed008181e53a4db70ffb93units=imperial';
 
 async function apiFetch() {
     try {
