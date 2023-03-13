e9bff6222275771d1821820a4b19777756e3d19e
Jruiz1980 <johnathanruiz@gmail.com>
2023-03-13 07:53:45 -0500

    add chill

--- w7/chamber/scripts/chill.js
+++ w7/chamber/scripts/chill.js
@@ -1,4 +1,4 @@
-const temp = document.querySelector("#temperature").innerHTML;
+const temp = document.querySelector("#current-temp").innerHTML;
 const wSpeed = document.querySelector("#windSpeed").innerHTML;
 var windChill = 35.74 + (0.6215 * temp)-35.75 * Math.pow(wSpeed,0.16) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
