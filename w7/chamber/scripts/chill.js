ddd28abaa2ac55d8a7029d3bc1517ec5ee32befe
Jruiz1980 <johnathanruiz@gmail.com>
2023-03-18 23:07:39 -0500

    add changes

--- w7/chamber/scripts/chill.js
+++ w7/chamber/scripts/chill.js
@@ -1,13 +1,11 @@
-const temp = document.querySelector("#current-temp").textContent;
-const wSpeed = document.querySelector("#windSpeed").textContent;
-const windChills = 35.74 + (0.6215 * temp) + (0.4275 * temp * (wSpeed**0.16))  - (35.75 * (wSpeed**0.16)) ;
+const temp = document.querySelector("#current-temp").innerHTML;
+const wSpeed = document.querySelector("#windSpeed").innerHTML;
+const windChill = 35.74 + (0.6215 * temp) + (0.4275 * temp * (wSpeed**0.16))  - (35.75 * (wSpeed**0.16));
 
-const windChill = Math.round(windChills);
-document.querySelector("#windChill").textContent = windChill;
-
-/*if (temp <= 52 && wSpeed > 3){
+document.querySelector("#windChill").innerHTML = windChill;
+if (temp <= 50 && wSpeed > 3){
 document.querySelector("#windChill").innerHTML= windChill;
 }
 else{
 document.querySelector("#windChill").innerHTML='N/A';
-}*/
+}
