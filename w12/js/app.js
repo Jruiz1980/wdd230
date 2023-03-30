4b5266e6af18735ac97fa45b5799aeab22a86cad
Jruiz1980 <johnathanruiz@gmail.com>
2023-03-29 22:42:15 -0500

    add

--- w12/js/app.js
+++ w12/js/app.js
@@ -1,6 +1,6 @@
+const x = document.getElementById('burguer-ico');
+    x.onclick = toggleMenu;
 function toggleMenu(){
-    document.getElementById("sidebar-ico").classList.toggle("open");
+    document.getElementById('sidebar-ico').classList.toggle("open");
     document.getElementById('burguer-ico').classList.toggle("open");
 }
-const x = document.getElementById('burguer-ico')
-    x.onclick = toggleMenu;
