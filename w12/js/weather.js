b31bb1ee79878cb71c44c06e28ba99b9e7712df6
Jruiz1980 <johnathanruiz@gmail.com>
2023-04-03 21:42:07 -0500

    add js

---
+++ w12/js/weather.js
@@ -0,0 +1,35 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector('#windSpeed');
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&units=imperial&appid=3f3f013e4fed008181e53a4db70ffb93';
+
+async function apiFetch() {
+    try {
+    const response = await fetch(url);
+    if (response.ok) {
+        const data = await response.json();
+        displayResults(data);
+        //console.log(data); // this is for testing the call
+    } else {
+        throw Error(await response.text());
+    }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+    windSpeed.textContent = weatherData.wind.speed.toFixed(0);
+}
