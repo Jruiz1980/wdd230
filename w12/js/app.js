5ea0fdc015283cc0332745d5e20f60f3e5f9a0d2
Jruiz1980 <johnathanruiz@gmail.com>
2023-04-08 21:27:50 -0500

    add changes

--- w12/js/app.js
+++ w12/js/app.js
@@ -9,3 +9,21 @@ let d = new Date();
     document.getElementById('currentYear').textContent = d.getFullYear();
 let lastUpdated = document.lastModified;
     document.getElementById('lastModified').textContent = lastUpdated;
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyBackgrounds = [].slice.call(document.querySelectorAll(".hero"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyBackgroundObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            lazyBackgroundObserver.unobserve(entry.target);
+            }
+        });
+    });
+    lazyBackgrounds.forEach(function(lazyBackground) {
+        lazyBackgroundObserver.observe(lazyBackground);
+    });
+    }
+});
